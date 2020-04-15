60e69f3760ca53fa1c6de4228a117b6b68722666
Ganeshc95 <ganeshengg95@gmail.com>
2020-04-15 13:36:13 +0530

    codeRefactor

---
+++ src/test/java/com/ganesh/makemyplan/MakemyplanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ganesh.makemyplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakemyplanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
